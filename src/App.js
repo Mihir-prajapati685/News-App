b3c080a6d20ea79b66928c9d2f924cb5debf8904
mihir-prajapati685 <mihirprajapati715@gmail.com>
2024-07-12 16:11:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Navbar from "./component/Navbar";
+import News from './component/News'
+import './App.css';
+
+
+function App() {
+    return (
+        <>
+            <Navbar/>
+            <News/>
+        </>
+    );
+}
+
+
+export default App;
